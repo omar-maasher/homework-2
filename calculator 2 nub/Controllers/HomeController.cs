47c98d9a22f9863aab592497ba6d9cbebf515079
omar-maasher <omar15448@gmail.com>
2023-05-15 18:55:29 +0300

    f1

---
+++ calculator 2 nub/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using calculator_2_nub.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace calculator_2_nub.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       
+        [HttpPost]
+        public IActionResult Index(int num1, int num2, string op,string op1)
+        {
+            int result = 0;
+            if (op == "+")
+            {
+                result = num1 + num2;
+            }
+            else if (op == "-")
+            {
+                result = num1 - num2;
+            }
+            else if (op == "/")
+            {
+                result = num1 / num2;
+            }
+            else if (op == "*")
+            {
+                result = num1 * num2;
+            }
+            op1 = op;
+            ViewBag.n1 = num1;
+            ViewBag.n2 = num2;
+
+            ViewBag.op = op1;
+            ViewBag.Result = result;
+            return View();
+        }
+
+    }
+}
---
+++ calculator 2 nub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace calculator_2_nub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
